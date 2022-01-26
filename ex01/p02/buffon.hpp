0c00648e61281b2d2fd4f0d0ff5dfc74c0c58514
lkotipal <leokotipalo@gmail.com>
2022-01-26 22:50:58 +0200

    Comments and seeding

--- ex01/p02/buffon.hpp
+++ ex01/p02/buffon.hpp
@@ -1,16 +1,20 @@
 #ifndef BUFFON_H
 #define BUFFON_H
 
-#include <random>
 #include <numbers>
+#include <random>
 
 class Buffon {
 public:
 	Buffon(double l, double d) : l(l), d(d), x_dis(0.0, d/2.0), phi_dis(0.0, std::numbers::pi/2) {}
+	void seed (std::uint_fast32_t seed) {rng.seed(seed);}
 	double simulate(unsigned int n);
 private:
+	// Simulate single needle dropped and return whether it falls on a line
 	bool needle_drop();
+	// Needle length
 	double l;
+	// Line distance
 	double d;
 	std::uniform_real_distribution<double> x_dis;
 	std::uniform_real_distribution<double> phi_dis;
--- ex01/p02/main.cpp
+++ ex01/p02/main.cpp
@@ -1,11 +1,26 @@
 #include <cmath>
 #include <iostream>
 #include <numbers>
+#include <string>
 #include "buffon.hpp"
 
-int main() 
+int main(int argc, char *argv[]) 
 {
+	std::uint_fast32_t seed = 0;
+	if (argc > 1) {
+		try {
+			seed = std::stoi(argv[1]);
+		} catch (std::invalid_argument e) {
+			std::cerr << "Invalid seed given." << std::endl;
+		}
+	} else {
+		std::cerr << "No seed given." << std::endl;
+	}
+	std::cerr << "Using seed " << seed << std::endl;
+
 	Buffon buffon(2.0, 10.0);
+	buffon.seed(seed);
+
 	for (int n = 10; n <= 10'000'000; n *= 10) {
 		double average_error = 0.0;
 		for (int i = 0; i < 100;) {
