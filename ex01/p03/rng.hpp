f0546dbbee78a675f34e3fe4c5ecb6c22449c633
lkotipal <leokotipalo@gmail.com>
2022-01-27 15:05:28 +0200

    Style

--- ex01/p03/rng.hpp
+++ ex01/p03/rng.hpp
@@ -7,10 +7,16 @@ class RNG {
 public:
 	virtual void seed(std::uint_fast32_t seed) = 0;
 	virtual bool has_seed(std::uint_fast32_t seed) = 0;
-	constexpr double inv_max() {return 1.0 / (max - min);}
-	double rand() {return (double) (rand_int() - min) * inv_max();}
+	double rand();
 private:
 	virtual std::uint_fast32_t rand_int() = 0;
+	static constexpr double inv_max {1.0 / (max - min)};
 };
+
+template <std::uint_fast32_t min, std::uint_fast32_t max>
+inline double RNG<min, max>::rand()
+{
+	return (rand_int() - min) * inv_max;
+}
 
 #endif
