858b392c85842c3dcd0fb38d54503d0ccfb444bf
lkotipal <leokotipalo@gmail.com>
2022-01-27 12:41:45 +0200

    Change Park-Miller constant

--- ex01/p03/pm.hpp
+++ ex01/p03/pm.hpp
@@ -10,7 +10,7 @@ private:
 	std::uint_fast32_t rand_int();
 	// Signed integers used for Schrage
 	std::int_fast32_t state {1};
-	static constexpr std::int_fast32_t a {48'271};	// Revised constant used in minstd_rand
+	static constexpr std::int_fast32_t a {16'807};	// Original Park-Miller minimal standard
 	static constexpr std::int_fast32_t m {2'147'483'647};
 	static constexpr std::int_fast32_t q {m / a};
 	static constexpr std::int_fast32_t r {m % a};
