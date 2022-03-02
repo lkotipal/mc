46b7cfe6d87acbddd31512d5f5f57dac2878017e
lkotipal <leokotipalo@gmail.com>
2022-03-02 21:57:18 +0200

    Problem 2 data

---
+++ kotipalo_leo_ex04/p02/plot.py
@@ -0,0 +1,21 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import describe
+
+for i in ('discrete', 'poisson'):
+	a = np.loadtxt(f'{i}.tsv')
+
+	(_, _, mean, var, _, _) = describe(a)
+	(low, high) = np.percentile(a, (16, 84))
+
+	print(f'{i}')
+	print(f'Mean {mean}')
+	print(f'Lower error {mean - low}, upper error {high - mean}')
+	print(f'Standard deviation {math.sqrt(var)}')
+	
+	plt.figure()
+	plt.hist(a, bins=math.ceil(math.log2(np.size(a))) + 1, density=True)
+	plt.xlabel(r'$\mu$')
+	plt.ylabel(r'$N$')
+	plt.savefig(f'{i}.png')
