34cf922822633a2658b61d1a2aca238265c3f165
lkotipal <leokotipalo@gmail.com>
2022-03-18 22:05:20 +0200

    aaa

--- advanced/kotipalo_leo_ex01/p04/main.cpp
+++ advanced/kotipalo_leo_ex01/p04/main.cpp
@@ -19,10 +19,9 @@ int main(int argc, char *argv[])
 	constexpr int n_trials = 100'000;
 	Self_avoiding_walker saw{0};
 
-	// For step size greater than 10000 double precision isn't enough for weights
-	//for (int n_steps = 1; n_steps <= 8192; n_steps *= 2) {
+	// We want results for N around 50
 	for (int n_steps = 1; n_steps <= 256; n_steps *= 2) {
-		double sum_r{0};
+		double sum_rw{0};
 		double sum_w{0};
 
 		double mean{0};
@@ -35,7 +34,7 @@ int main(int argc, char *argv[])
 			double prev_mean = mean;
 
 			// For calculating weighted mean
-			sum_r += result.r_squared * result.weight;
+			sum_rw += result.r_squared * result.weight;
 			sum_w += result.weight;
 
 			// For calculating standard deviation
@@ -44,20 +43,14 @@ int main(int argc, char *argv[])
 			m_sq += (result.r_squared - prev_mean) * (result.r_squared - mean);
 		}
 
-		// Variance
+		// Weighted variance
 		double w_squared{0};
 		for (double w : weights) {
 			w_squared += std::pow(w / sum_w, 2);
 		}
 
-		std::cout << std::scientific << n_steps << "\t" << sum_r / sum_w << "\t" << std::sqrt(m_sq / (n_trials - 1) * w_squared) << std::endl;
-		//std::cout << std::scientific << n_steps << "\t" << mean << "\t" << std::sqrt(m_sq / (n_trials - 1)) / sqrt(n_trials) << std::endl;
+		std::cout << std::scientific << n_steps << "\t" << sum_rw / sum_w << "\t" << std::sqrt(m_sq / (n_trials - 1) * w_squared) << std::endl;
 	}
-
-	//for (int i = 0; i < n_trials; ++i) {
-	//	Walker_result result = saw.simulate(50);
-	//	std::cout << result.r_squared << "\t" << result.weight << std::endl;
-	//}
 	
 	return 0;
 }
